23129d383ffabeb0a889344cfe73898003634d18
Melissa Pürstinger <puerstinger.melissa@gmail.com>
2025-01-23 11:41:50 +0100

    Adjust CSS file

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # React Meme Generator
 
 Create a web app with React that allows for users to generate and download memes using the https://memegen.link/ website.
+
+## TO DOs
+
+-[]Find a way to connect our meme link with react using API
+-[]Set up a design idea
+-[]Find a way to fetch memes and show them in our application
+-[]Create a template users can use to preview meme they want to create
+-[]Add input fields for top text and bottom text
+-[]Connect text to selected meme via template
+-[]Create preview
+-[]Create option to download created meme
--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import './App.css'; // Ensure the CSS file is imported
+import './App.css';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import Select, { components } from 'react-select';
 
-// Custom dropdown option with icons
 const IconOption = (props) => {
   const { Option } = components;
   return (
@@ -24,10 +23,9 @@ function App() {
   const [topText, setTopText] = useState('');
   const [bottomText, setBottomText] = useState('');
   const [memeUrl, setMemeUrl] = useState(
-    'https://api.memegen.link/images/doge.png', // Default image
+    'https://api.memegen.link/images/doge.png',
   );
 
-  // Fetch meme templates from the API
   useEffect(() => {
     axios
       .get('https://api.memegen.link/templates')
@@ -39,7 +37,6 @@ function App() {
       });
   }, []);
 
-  // Generate meme URL
   const handleGenerateClick = () => {
     const formattedTopText = topText.trim().replaceAll(' ', '_') || '_';
     const formattedBottomText = bottomText.trim().replaceAll(' ', '_') || '_';
@@ -52,7 +49,6 @@ function App() {
     setMemeUrl(newMemeUrl);
   };
 
-  // Download the generated meme
   const handleDownloadClick = () => {
     const link = document.createElement('a');
     link.href = memeUrl;
@@ -62,7 +58,6 @@ function App() {
     document.body.removeChild(link);
   };
 
-  // Map templates into options for the dropdown
   const options = templates.map((template) => ({
     value: template.id,
     label: template.name,
@@ -73,35 +68,38 @@ function App() {
     <div className="Location">
       <h1>Meme Generator</h1>
 
-      {/* Display the generated meme */}
       <a href={memeUrl} download>
-        <img src={memeUrl} alt="Generated Meme" />
+        <img src={memeUrl} alt="Generated Meme" data-test-id="meme-image" />
       </a>
 
-      {/* Dropdown for selecting meme templates */}
       <div className="select">
         <Select
           options={options}
           components={{ Option: IconOption }}
           onChange={setSelectedTemplate}
-          placeholder="Select a meme template"
+          placeholder="Meme template"
         />
       </div>
 
-      {/* Input fields for top and bottom text */}
+      <label htmlFor="topText">Top text</label>
       <input
-        placeholder="Enter Top Text"
+        id="topText"
         value={topText}
         onChange={(e) => setTopText(e.target.value)}
       />
+
+      <label htmlFor="bottomText">Bottom text</label>
       <input
-        placeholder="Enter Bottom Text"
+        id="bottomText"
         value={bottomText}
         onChange={(e) => setBottomText(e.target.value)}
       />
 
-      {/* Buttons */}
-      <button onClick={handleGenerateClick} disabled={!selectedTemplate}>
+      <button
+        onClick={handleGenerateClick}
+        disabled={!selectedTemplate}
+        data-test-id="generate-meme"
+      >
         Generate Meme
       </button>
       <button onClick={handleDownloadClick}>Download Meme</button>
