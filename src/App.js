380f6530f01d1ef6a4714b3f8d6e7a956be85112
Melissa Pürstinger <puerstinger.melissa@gmail.com>
2025-01-23 13:41:32 +0100

    Adjust template labels

--- src/App.js
+++ src/App.js
@@ -72,17 +72,19 @@ function App() {
         <img src={memeUrl} alt="Generated Meme" data-test-id="meme-image" />
       </a>
 
-      <label htmlFor="meme-template" className="meme-template-label">
-        Meme Template
-      </label>
-      <div className="select">
-        <Select
-          id="meme-template"
-          options={options}
-          components={{ Option: IconOption }}
-          onChange={setSelectedTemplate}
-          placeholder="Meme template"
-        />
+      <div className="select-container">
+        <label htmlFor="meme-template" className="meme-template-label">
+          Meme Template
+        </label>
+        <div className="select">
+          <Select
+            id="meme-template"
+            options={options}
+            components={{ Option: IconOption }}
+            onChange={setSelectedTemplate}
+            placeholder="Meme template"
+          />
+        </div>
       </div>
 
       <label htmlFor="topText">Top text</label>
